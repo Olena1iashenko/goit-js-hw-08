82ef11c940fc4e35b2f3f4f02b2b2f211f5c6537
Olena1iashenko <lyashenko.alenkaa@gmail.com>
2024-04-01 09:00:50 +0300

    hw8 v0

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const liItemElements = document.querySelectorAll(".item");
-console.log(`Number of catgories: ${liItemElements.length}`);
-
-liItemElements.forEach(el => {
-    const subTitle = el.querySelector('h2').textContent;
-    console.log(`Category: ${subTitle}`);
-    const liItems = el.querySelectorAll('li').length;
-    console.log(`Elements: ${liItems}`);
-});
